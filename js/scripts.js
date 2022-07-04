2022ed527002b97569e68417bf24e3bd388311df
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2022-07-04 11:07:12 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,34 @@
 $(function() {
     
     const swiper = new Swiper('.swiper', {
-      // Optional parameters
-      loop: true,
+    	// Optional parameters
+    	loop: true,
+    	slidesPerView: 2,
+    	// cssMode: true,
+    	// mousewheel: true,
+    	// keyboard: true,
+    	// noSwiping: false,
+    	// autoplay: {
+    	// 	delay: 2500,
+    	// 	disableOnInteraction: false,
+    	// },
+    	breakpoints: {
+    		992: {
+    			slidesPerView: 3,
+    			// spaceBetween: 40,
+    		},
+    		1200: {
+    			slidesPerView: 4,
+    			// spaceBetween: 20,
+    		},
+    	},
+    });
     
-      // Navigation arrows
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
+    $('.slider-button-prev').on('click', function(){
+    	swiper.slidePrev();
+    });
+    
+    $('.slider-button-next').on('click', function(){
+    	swiper.slideNext();
     });
 });
