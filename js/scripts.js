78d4d680f635432ee85cc4f7607c2957720afefb
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2022-07-09 09:34:32 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -2,88 +2,90 @@ $(function() {
 
 	$('.wp-block-column').has('.wp-block-image').addClass( 'order' );
 
-    
-    const modelsSlider = new Swiper('.models-slider', {
-    	// Optional parameters
-    	loop: true,
-    	slidesPerView: 1,
-    	// cssMode: true,
-    	// mousewheel: true,
-    	// keyboard: true,
-    	// noSwiping: false,
-    	// allowTouchMove: false,
-    	autoplay: {
-    		delay: 2500,
-    		disableOnInteraction: false,
-    	},
-    	breakpoints: {
-    		992: {
-    			slidesPerView: 3,
-    			// spaceBetween: 40,
-    			// allowTouchMove: true,
-    		},
-    		1200: {
-    			slidesPerView: 4,
-    			// allowTouchMove: true,
-    			// spaceBetween: 20,
-    		},
-    	},
-    });
-    
-    $('.models-button-prev').on('click', function(){
-    	modelsSlider.slidePrev();
-    });
-    
-    $('.models-button-next').on('click', function(){
-    	modelsSlider.slideNext();
-    });
-    
-    const feedbackSlider = new Swiper('.feedback-slider', {
-    	// loop: true,
-    	slidesPerView: 1,
-    	spaceBetween: 30,
-    	autoplay: {
-    		delay: 2500,
-    		disableOnInteraction: false,
-    	},
-    	breakpoints: {
-    		678: {
-    			slidesPerView: 2,
-    		},
-    		1010: {
-    			slidesPerView: 3,
-    		},
-    		1350: {
-    			slidesPerView: 4,
-    		},
-    	},
-    });
-    
-    $('.feedback-button-prev').on('click', function(){
-    	feedbackSlider.slidePrev();
-    });
-    
-    $('.feedback-button-next').on('click', function(){
-    	feedbackSlider.slideNext();
-    });
-    
-    // var swiper = new Swiper(".mySwiper", {
-    // 	spaceBetween: 10,
-    // 	slidesPerView: 4,
-    // 	freeMode: true,
-    // 	watchSlidesProgress: true,
-    // });
-    // var swiper2 = new Swiper(".mySwiper2", {
-    // 	spaceBetween: 10,
-    // 	navigation: {
-    // 		nextEl: ".swiper-button-next",
-    // 		prevEl: ".swiper-button-prev",
-    // 	},
-    // 	thumbs: {
-    // 		swiper: swiper,
-    // 	},
-    // });
-    Fancybox.bind("[data-fancybox]", {
-      // Your options go here
-    });
+	$('select').niceSelect();
+
+	
+	const modelsSlider = new Swiper('.models-slider', {
+		// Optional parameters
+		loop: true,
+		slidesPerView: 1,
+		// cssMode: true,
+		// mousewheel: true,
+		// keyboard: true,
+		// noSwiping: false,
+		// allowTouchMove: false,
+		autoplay: {
+			delay: 2500,
+			disableOnInteraction: false,
+		},
+		breakpoints: {
+			992: {
+				slidesPerView: 3,
+				// spaceBetween: 40,
+				// allowTouchMove: true,
+			},
+			1200: {
+				slidesPerView: 4,
+				// allowTouchMove: true,
+				// spaceBetween: 20,
+			},
+		},
+	});
+	
+	$('.models-button-prev').on('click', function(){
+		modelsSlider.slidePrev();
+	});
+	
+	$('.models-button-next').on('click', function(){
+		modelsSlider.slideNext();
+	});
+	
+	const feedbackSlider = new Swiper('.feedback-slider', {
+		// loop: true,
+		slidesPerView: 1,
+		spaceBetween: 30,
+		autoplay: {
+			delay: 2500,
+			disableOnInteraction: false,
+		},
+		breakpoints: {
+			678: {
+				slidesPerView: 2,
+			},
+			1010: {
+				slidesPerView: 3,
+			},
+			1350: {
+				slidesPerView: 4,
+			},
+		},
+	});
+	
+	$('.feedback-button-prev').on('click', function(){
+		feedbackSlider.slidePrev();
+	});
+	
+	$('.feedback-button-next').on('click', function(){
+		feedbackSlider.slideNext();
+	});
+	
+	var swiper = new Swiper(".mySwiper", {
+		spaceBetween: 10,
+		slidesPerView: 4,
+		freeMode: true,
+		watchSlidesProgress: true,
+	});
+	var swiper2 = new Swiper(".mySwiper2", {
+		spaceBetween: 10,
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+		thumbs: {
+			swiper: swiper,
+		},
+	});
+	Fancybox.bind("[data-fancybox]", {
+	  // Your options go here
+	});
 });
